d9c6e962e75a1f7554f21105eadbfc1a709a56fe
sleebapaul <sleebapaul@gmail.com>
2018-01-13 14:21:31 +0530

    cleaned xcode mistakes

--- file_stream.cpp
+++ file_stream.cpp
@@ -11,33 +11,36 @@
 #include <string>
 using namespace std;
 
-int main () {
+int main()
+{
     string line;
     //create an output stream to write to the file
     //append the new lines to the end of the file
-    ofstream myfileI ("input.txt", ios::app);
+    ofstream myfileI("input.txt", ios::app);
     if (myfileI.is_open())
     {
         myfileI << "\nI am adding a line.\n";
         myfileI << "I am adding another line.\n";
         myfileI.close();
     }
-    else cout << "Unable to open file for writing";
-    
+    else
+        cout << "Unable to open file for writing";
+
     //create an input stream to read the file
-    ifstream myfileO ("input.txt");
+    ifstream myfileO("input.txt");
     //During the creation of ifstream, the file is opened.
     //So we do not have explicitly open the file.
     if (myfileO.is_open())
     {
-        while ( getline (myfileO,line) )
+        while (getline(myfileO, line))
         {
             cout << line << '\n';
         }
         myfileO.close();
     }
-    
-    else cout << "Unable to open file for reading";
-    
+
+    else
+        cout << "Unable to open file for reading";
+
     return 0;
 }
